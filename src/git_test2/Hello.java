b5a5e9169d5394b4b152c2d8df8c1120e9ce46ea
Park, minsu <cmwmss54@gmail.com>
2022-09-26 12:32:24 +0900

    Hello 클래스 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -1,7 +1,15 @@
 package git_test2;
 
 public class Hello {
-	String message="ì•ˆë…•í•˜ì„¸ìš”";
+	String message;
+	public Hello()
+	{
+		this("¾È³çÇÏ¼¼¿ä");
+	}
+	public Hello(String message)
+	{
+		this.message=message;
+	}
 	public void say()
 	{
 		System.out.println(message);
